567060f8c844135e0dce536e907af1e5f3babbac
pushpa-u <pushpa.u@10decoders.in>
2024-07-19 13:11:57 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       
-      <h1>Hello World!Push into docker hub with incremental build version</h1>
+      <h1>Hello World!Push into docker hub with incremental build version.Scondversion</h1>
     </div>
   );
 }
