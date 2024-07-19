49fe23396a457cc26b1993b370d28aa895fbb6ec
pushpa-u <pushpa.u@10decoders.in>
2024-07-19 15:02:06 +0530

    Private repository created in HUB and updated

--- increment_version.sh
+++ increment_version.sh
@@ -10,8 +10,5 @@ current_version=$(cat $VERSION_FILE)
 new_version=$((current_version + 1))
 echo $new_version > $VERSION_FILE
 
-echo "Current version: $current_version"
-echo "New version: $new_version"
-
 # Output the new version to be used in GitHub Actions
 echo "::set-output name=new_version::$new_version"
